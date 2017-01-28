13b726174b42b34e3d40211c9aea9f6bc2d5e4c3
Shekhar Gulati <shekhargulati84@gmail.com>
2017-01-28 16:30:44 +0530

    Added @FunctionalInterface annotation on interfaces

--- src/main/java/imageresolver/HtmlToMainImageResolver.java
+++ src/main/java/imageresolver/HtmlToMainImageResolver.java
@@ -3,5 +3,6 @@ package imageresolver;
 import java.util.Optional;
 import java.util.function.Function;
 
+@FunctionalInterface
 public interface HtmlToMainImageResolver extends Function<HtmlDoc, Optional<String>> {
 }
--- src/main/java/imageresolver/UrlToHtml.java
+++ src/main/java/imageresolver/UrlToHtml.java
@@ -2,5 +2,6 @@ package imageresolver;
 
 import java.util.function.Function;
 
+@FunctionalInterface
 interface UrlToHtml extends Function<String, HtmlDoc> {
 }
