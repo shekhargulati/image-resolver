553c87bf44e1ca126599a99d676184c3088de3f5
Shekhar Gulati <shekhargulati84@gmail.com>
2017-02-04 15:42:33 +0530

    Added logging for http calls

--- src/main/java/imageresolver/OkHttpBasedUrlToHtml.java
+++ src/main/java/imageresolver/OkHttpBasedUrlToHtml.java
@@ -3,6 +3,8 @@ package imageresolver;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.util.HashMap;
@@ -11,6 +13,8 @@ import java.util.Map;
 
 class OkHttpBasedUrlToHtml implements UrlToHtml {
 
+    private static final Logger logger = LoggerFactory.getLogger(OkHttpBasedUrlToHtml.class);
+
     private final OkHttpClient client = new OkHttpClient();
 
     private final Map<String, HtmlDoc> cache = new HashMap<>();
@@ -18,9 +22,11 @@ class OkHttpBasedUrlToHtml implements UrlToHtml {
     @Override
     public HtmlDoc apply(final String url) {
         if (cache.containsKey(url)) {
+            logger.info("Returning HTML from the cache");
             return cache.get(url);
         }
         try {
+            logger.info("Making a call to fetch HTML");
             final Response response = client.newCall(new Request.Builder().url(url).get().build()).execute();
             Map<String, List<String>> headers = response.headers().toMultimap();
             String html = response.body().string();
--- src/test/java/imageresolver/resolvers/Tmp.java
+++ src/test/java/imageresolver/resolvers/Tmp.java
@@ -3,22 +3,31 @@ package imageresolver.resolvers;
 import okhttp3.Call;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
-import okhttp3.Response;
 
 import java.io.IOException;
+import java.util.stream.IntStream;
 
 public class Tmp {
 
     public static void main(String[] args) throws IOException {
-        long start = System.currentTimeMillis();
+        IntStream.range(0, 9).forEach(i -> {
+            try {
+                makeCall();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
+
+    }
+
+    public static void makeCall() throws IOException {
         OkHttpClient client = new OkHttpClient();
-        Call call = client.newCall(new Request.Builder().head().url("https://upload.wikimedia.org/wikipedia/commons/3/3f/JPEG_example_flower.jpg").build());
-        Response response = call.execute();
+        Request request = new Request.Builder().get().url("https://www.forbes.com/sites/startswithabang/2017/01/24/nobody-knows-where-a-black-holes-information-goes/").build();
+        long start = System.currentTimeMillis();
+        Call call = client.newCall(request);
+        call.execute();
         long end = System.currentTimeMillis();
         System.out.println(String.format("Total time: %d sec(s)", (end - start) / 1000));
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println(response.headers());
     }
 }
